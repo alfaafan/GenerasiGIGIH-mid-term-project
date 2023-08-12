3cc0b461b8ed0d8fbf78ded7c140c210bc7cb5db
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-08-12 09:51:44 +0700

    enable cors in backend

--- api/index.js
+++ api/index.js
@@ -3,8 +3,11 @@ import express from "express";
 import mongoose from "mongoose";
 const port = process.env.PORT || 3000;
 import router from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL).catch((error) => handleError(error));
 const db = mongoose.connection;
