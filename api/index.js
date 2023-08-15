d3a224e529c82fb8a3d3c54e31db59924c7347ba
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-08-15 09:50:22 +0700

    fix mongoose connect

--- api/index.js
+++ api/index.js
@@ -11,8 +11,8 @@ app.use(cors());
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected`);
   } catch (error) {
     console.log(error);
     process.exit(1);
